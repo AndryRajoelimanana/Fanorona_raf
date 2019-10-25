8628d25cb1eed836c03b7f972e06c0593b7ab1ad
AndryRajoelimanana <AndryRajoelimanana@gmail.com>
2019-10-25 18:33:08 -0400

    Starting with the Evaluation code

--- Board/Evaluation.py
+++ Board/Evaluation.py
@@ -3,30 +3,29 @@ from Utils.utils import *
 
 
 class Evaluation():
-
     active_squares = 92632243238996
     # terms for broken-fortress evaluation
-    attack_weight = 600         # value of a successful attack
+    attack_weight = 600  # value of a successful attack
     trapped_piece_weight = 100  # value of a trapped piece
-    conversion_weight = 35      # bonus for converting to simpler pos
-    max_positional_eval = 50    # expected max of next two terms
-    forward_weight = 10         # value of a piece in the attacking zone
-    space_weight = 1            # penalty for allowing defense liebensraum
+    conversion_weight = 35  # bonus for converting to simpler pos
+    max_positional_eval = 50  # expected max of next two terms
+    forward_weight = 10  # value of a piece in the attacking zone
+    space_weight = 1  # penalty for allowing defense liebensraum
 
     # terms for eval of relatively even positions
-    piece_weight = 300          # multiplier for material ratio
-    piece_value = 100           # add this number to ratio of activeposn
-    attack_bonus = 50           # one side attacking but cant break fort
+    piece_weight = 300  # multiplier for material ratio
+    piece_value = 100  # add this number to ratio of activeposn
+    attack_bonus = 50  # one side attacking but cant break fort
 
     # extra depths to search if a capture is available on this or the next ply
     capture_depth = -30
     threat_depth = -15
 
     # bit masks for finding batteries
-    left_margin = 211346952257728    # must be clear for battery
-    left_battery = 262400            # must be set in battery test
-    right_margin = 6612108457990     # must be clear for battery
-    right_battery = 1025             # must be set in battery test
+    left_margin = 211346952257728  # must be clear for battery
+    left_battery = 262400  # must be set in battery test
+    right_margin = 6612108457990  # must be clear for battery
+    right_battery = 1025  # must be set in battery test
 
     # stuff for finding fortresses
     sm_left_fort = 281887696617728
@@ -46,32 +45,41 @@ class Evaluation():
     left_control = 171865964544
     right_control = 10741622784
     center_control = 42966491136
-    ratios = [2147483647]+[int(piece_weight/(i+1)) for i in range(64)]
+    ratios = [2147483647] + [int(piece_weight / (i + 1)) for i in range(64)]
 
     @staticmethod
     def attack(attackers, open_on_board):
-        moves = getMoves(attackers, open_on_board, Bits.shift_vertical)
 
+        shift_type = Bits.shift_vertical
+        moves = getMoves(attackers, open_on_board, shift_type)
+        unsafe_pieces = eaten_pieces(moves, shift_type)
 
-        moves = (rshift(attackers, Bits.shift_vertical) & openb)| (rshift(openb, Bits.shift_vertical) & attackers)
-        unsafe = rshift(moves, Bits.shift_vertical) | (moves << (2 * Bits.shift_vertical))
-        moves = (rshift(attackers, Bits.shift_horizontal) & openb) | (rshift(openb, Bits.shift_horizontal) & attackers)
-        unsafe |= rshift(moves, Bits.shift_horizontal) | (moves << (2 * Bits.shift_horizontal))
+        shift_type = Bits.shift_horizontal
+        moves = getMoves(attackers, open_on_board, shift_type)
+        unsafe_pieces |= eaten_pieces(moves, shift_type)
+
         attackers &= Bits.diagonal
-        moves = (rshift(attackers, Bits.shift_slant) & openb)| (rshift(openb, Bits.shift_slant) & attackers)
-        unsafe |= rshift(moves, Bits.shift_slant) | (moves << (2 * Bits.shift_slant))
-        moves = (rshift(attackers, Bits.shift_backslant) & openb)| (rshift(openb, Bits.shift_backslant) & attackers)
-        return unsafe | (rshift(moves, Bits.shift_backslant) | (moves << (2 * Bits.shift_backslant)))
+
+        shift_type = Bits.shift_slant
+        moves = getMoves(attackers, open_on_board, shift_type)
+        unsafe_pieces |= eaten_pieces(moves, shift_type)
+
+        shift_type = Bits.shift_backslant
+        moves = getMoves(attackers, open_on_board, shift_type)
+        unsafe_pieces |= eaten_pieces(moves, shift_type)
+
+        return unsafe_pieces
 
     @staticmethod
     def nextTo(s):
         n = (rshift(s, Bits.shift_vertical)) | (s << Bits.shift_vertical)
-        s |= (n &  ~Bits.diagonal)
+        s |= (n & ~Bits.diagonal)
         return n | (rshift(s, Bits.shift_horizontal)) | (s << Bits.shift_horizontal)
 
     @staticmethod
     def activity(pieces, safeMoves):
-        act = (Evaluation.active_squares & Evaluation.nextTo(safeMoves)) | Evaluation.nextTo(Evaluation.active_squares & safeMoves)
+        act = (Evaluation.active_squares & Evaluation.nextTo(safeMoves)) | Evaluation.nextTo(
+            Evaluation.active_squares & safeMoves)
         return (act | (Evaluation.active_squares & Evaluation.nextTo(act & pieces))) & pieces
 
     @staticmethod
@@ -81,12 +89,12 @@ class Evaluation():
         opponentPieces = b.opponentPieces & Bits.on_board
         myPieceCount = Bits.count(myPieces)
         oppPieceCount = Bits.count(opponentPieces)
-        if ((myPieceCount <= 2) and (oppPieceCount <=2)):
+        if ((myPieceCount <= 2) and (oppPieceCount <= 2)):
             return True
 
         open_position = open_on_board(b)
         myAttacks = Evaluation.attack(myPieces, open_position)
-        if ((myAttacks & opponentPieces) !=0):
+        if ((myAttacks & opponentPieces) != 0):
             oppPieceCount -= 1
             if (oppPieceCount == 0):
                 b.evaluation = myPieceCount * Board.won_position - Board.ply_decrement
@@ -94,19 +102,21 @@ class Evaluation():
             if (depth > Evaluation.capture_depth):
                 return False
             if (myPieceCount >= oppPieceCount):
-                b.evaluation = (myPieceCount - oppPieceCount) * (Evaluation.ratios[oppPieceCount] + Evaluation.piece_value)
+                b.evaluation = (myPieceCount - oppPieceCount) * (
+                            Evaluation.ratios[oppPieceCount] + Evaluation.piece_value)
             else:
-                b.evaluation = -(oppPieceCount - myPieceCount) * (Evaluation.ratios[myPieceCount] + Evaluation.piece_value)
+                b.evaluation = -(oppPieceCount - myPieceCount) * (
+                            Evaluation.ratios[myPieceCount] + Evaluation.piece_value)
             return True
 
         oppAttacks = Evaluation.attack(opponentPieces, open_position)
         mySafeMoves = open_position & ~oppAttacks
         myActive = Evaluation.activity(myPieces, mySafeMoves)
         attacked = myPieces & oppAttacks
-        if ((myActive == 0) or ((attacked & myActive) != attacked) or ((attacked & (attacked -1)) != 0)):
+        if ((myActive == 0) or ((attacked & myActive) != attacked) or ((attacked & (attacked - 1)) != 0)):
             myPieceCount -= 1
             if (myPieceCount == 0):
-                b.evaluation = -(oppPieceCount * Board.won_position - 4*Board.ply_decrement)
+                b.evaluation = -(oppPieceCount * Board.won_position - 4 * Board.ply_decrement)
                 return True
             if (depth > Evaluation.capture_depth):
                 return False
@@ -161,11 +171,11 @@ class Evaluation():
             alpha = -beta
             beta = -x
         if (((defendingActivity + defendingTrapped) == 1)
-            and (attackingActivity >= 2) and (attackingTrapped == 0)
-            and ((safeForDefense & Evaluation.nextTo(attackingPieces &
-             ~Evaluation.active_squares)) == 0)
-             and ((safeForDefense & Evaluation.nextTo(attackingPieces) &
-              Evaluation.nextTo(defendingPieces)) == 0)):
+                and (attackingActivity >= 2) and (attackingTrapped == 0)
+                and ((safeForDefense & Evaluation.nextTo(attackingPieces &
+                                                         ~Evaluation.active_squares)) == 0)
+                and ((safeForDefense & Evaluation.nextTo(attackingPieces) &
+                      Evaluation.nextTo(defendingPieces)) == 0)):
             b.evaluation = int(attackingActivity * Board.won_position - (Board.won_position / 2) + control)
             if not (attacking):
                 b.evaluation = -b.evaluation
@@ -175,7 +185,7 @@ class Evaluation():
         fortress = 0
         fortressStrength = 0
         if (((Evaluation.lg_left_fort & attackingPieces) == 0)
-            and ((Evaluation.lg_left_guard & defendingPieces) != 0)):
+                and ((Evaluation.lg_left_guard & defendingPieces) != 0)):
             print('tato lg_left')
             fortress = Evaluation.lg_left_fort & defendingPieces
             fortress &= fortress - 1
@@ -191,7 +201,7 @@ class Evaluation():
                     defendingTrapped -= 1
 
         elif (((Evaluation.sm_left_fort & attackingPieces) == 0)
-            and ((Evaluation.sm_left_guard & defendingPieces) != 0)):
+              and ((Evaluation.sm_left_guard & defendingPieces) != 0)):
             print('tato sm_left')
             fortress = Evaluation.sm_left_fort & defendingPieces
             fortress &= fortress - 1;
@@ -203,7 +213,8 @@ class Evaluation():
                     defendingTrapped -= 1
 
         # large and small right fortresses
-        if (((Evaluation.lg_right_fort & attackingPieces) == 0) and ((Evaluation.lg_right_guard & defendingPieces) != 0)):
+        if (((Evaluation.lg_right_fort & attackingPieces) == 0) and (
+                (Evaluation.lg_right_guard & defendingPieces) != 0)):
             print('tato lg_right')
             fortress = Evaluation.lg_right_fort & defendingPieces
             fortress &= fortress - 1;
@@ -219,7 +230,7 @@ class Evaluation():
                     defendingTrapped -= 1
 
         elif (((Evaluation.sm_right_fort & attackingPieces) == 0)
-            and ((Evaluation.sm_right_guard & defendingPieces) != 0)):
+              and ((Evaluation.sm_right_guard & defendingPieces) != 0)):
             print('tato sm_right')
             fortress = Evaluation.sm_right_fort & defendingPieces
             fortress &= fortress - 1
@@ -234,7 +245,9 @@ class Evaluation():
             fortressStrength = rshift(control, 31)
 
         if (attackingActivity - defendingActivity - fortressStrength > 0):
-            evalb = Evaluation.attack_weight * (attackingActivity - defendingActivity-fortressStrength) + Evaluation.trapped_piece_weight *(attackingTrapped - defendingTrapped) - Evaluation.conversion_weight * defendingActivity
+            evalb = Evaluation.attack_weight * (
+                        attackingActivity - defendingActivity - fortressStrength) + Evaluation.trapped_piece_weight * (
+                                attackingTrapped - defendingTrapped) - Evaluation.conversion_weight * defendingActivity
         else:
             a = 2 * attackingActivity + attackingTrapped
             d = 2 * defendingActivity + defendingTrapped
@@ -249,17 +262,16 @@ class Evaluation():
         if ((evalb + Evaluation.max_positional_eval > alpha) and (evalb - Evaluation.max_positional_eval < beta)):
             space = defendingPieces
             while True:
-                newSpace =  space | ((Evaluation.nextTo(space) & safeForDefense))
+                newSpace = space | ((Evaluation.nextTo(space) & safeForDefense))
                 if (space == newSpace):
                     break
-                space =  newSpace
+                space = newSpace
 
-            evalb += Evaluation.forward_weight * Bits.count(attackingPieces & attackZone) - Evaluation.space_weight * Bits.count(space)
+            evalb += Evaluation.forward_weight * Bits.count(
+                attackingPieces & attackZone) - Evaluation.space_weight * Bits.count(space)
 
-        if(attacking):
+        if (attacking):
             b.evaluation = evalb
         else:
             b.evaluation = -evalb
         return True
-
-
--- Utils/utils.py
+++ Utils/utils.py
@@ -2,7 +2,7 @@ from Utils.Bits import Bits
 
 
 def PiecesOnBoard(pieces, pieces2):
-    '''Returns pieces array'''
+    """Returns pieces array"""
     stones = [[0] * 9, [0] * 9, [0] * 9, [0] * 9, [0] * 9]
     for i in range(5):
         for j in range(9):
@@ -16,32 +16,37 @@ def PiecesOnBoard(pieces, pieces2):
 
 
 def ShowBoard(pieces, pieces2):
-    '''Showing pieces on the Board'''
-    boardpieces = PiecesOnBoard(pieces, pieces2)
+    """Showing pieces on the Board"""
+    board_pieces = PiecesOnBoard(pieces, pieces2)
     ff = '\nmyPieces : %s \noppPieces : %s \n \n' % (pieces, pieces2)
-    for i in range(5): ff = ff + '  ' + '  '.join(boardpieces[i][:]) + '\n'
+    for i in range(5): ff = ff + '  ' + '  '.join(board_pieces[i][:]) + '\n'
     return ff
 
+
 def open_on_board(board):
-    occupied = board.myPieces | board.pponentPieces
+    occupied = board.myPieces | board.opponentPieces
     open_position = Bits.on_board & ~occupied
     return open_position
 
 
+def eaten_pieces(moves, movetype):
+    return rshift(moves, movetype) | (moves << (2 * movetype))
+
+
 def getMoves(attackers, open_on_board, movetype):
-    '''Returns moves for a specific move type'''
+    """Returns moves for a specific move type"""
     return (attackers & rshift(open_on_board, movetype)) | (open_on_board & rshift(attackers, movetype))
 
 
 def NegBit(val):
-    '''Returs Negative Bit'''
+    """Returs Negative Bit"""
     if (val | (1 << 63)) > (1 << 63):
         val = val - (1 << 63)
     return val
 
 
 def PosBit(val):
-    '''Returs Positive Bit'''
+    """Returs Positive Bit"""
     if (val & (~(1 << 63))) < -(1 << 63):
         val = val + (1 << 63)
     return val
