bbcab1363c51fcd62d50f06cb54470c04b5489b6
AndryRajoelimanana <AndryRajoelimanana@gmail.com>
2019-11-29 23:35:35 -0500

    Fully working version flask with controled depth

--- Fanorona/fanorona.py
+++ Fanorona/fanorona.py
@@ -8,8 +8,6 @@ from flask_cors import CORS
 app = Flask(__name__, template_folder="static/react/")
 CORS(app)
 
-
-
 app.secret_key = '\xb3j\x9e\xeeD&g\xa4\xcd\xdeH\xa4\x0fT\xc4+\xdf\x04H{_\xa0\xe0g'
 
 
@@ -25,19 +23,16 @@ def pass_game():
     in_board = request.get_json()
     board = in_board['boardstate']
     was_capture = in_board['was_capture']
+    depth = int(in_board['depth'])
     my_pieces, opp_pieces = utils.board_to_bit(board)
-    my_board = SetBoard(my_pieces, opp_pieces)
-    # my_board = Boardmove(my_board, 0)
+    my_board = SetBoard(my_pieces, opp_pieces, was_capture)
     print(my_board.myPieces, my_board.opponentPieces)
     move_log = []
     movedict = dict()
-    # search_obj = Search(my_board, ply=3)
-    # move_log.append(search_obj.board.best_move)
     b_search = my_board
     while not b_search.human_to_move():
-        new_search = Search(b_search, ply=3)
+        new_search = Search(b_search, ply=depth)
         if new_search.board.forced:
-            print('tato')
             move = new_search.board.arbmove
         else:
             move = new_search.board.best_move
